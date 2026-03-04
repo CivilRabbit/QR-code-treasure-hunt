ae56440f269db18fd318cbfeeca990a019441aad
Vincent Boon <vincentboon2004@gmail.com>
2026-03-04 19:53:23 +0100

    update to pupups

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 
+let popup: HTMLElement;
+
 export function check(correct: Boolean){
+    if (popup && popup.classList.contains("show")) popup.classList.toggle("show")
     if (correct){
-        console.log("hint")
+        popup = document.getElementById("succes")!;
     } else{
-        console.log("try again bitch")
+        popup = document.getElementById("failure")!;
     }
+    popup.classList.toggle("show");
 }
